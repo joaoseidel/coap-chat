711c0e51fe77909837f262590ce241a13ba9d8bb
Seidel <joao.santos2@fluig.com>
2020-04-12 03:07:25 -0300

    :loud_sound: Add log configuration to the server

--- server/src/main/java/fun/seidel/Server.java
+++ server/src/main/java/fun/seidel/Server.java
@@ -2,12 +2,13 @@ package fun.seidel;
 
 import java.util.UUID;
 
+import org.apache.log4j.BasicConfigurator;
 import org.eclipse.californium.core.CoapServer;
 
 import fun.seidel.cache.Groups;
 import fun.seidel.model.Group;
-import fun.seidel.resources.GroupResource;
 import fun.seidel.resources.GroupMessageResource;
+import fun.seidel.resources.GroupResource;
 
 public class Server extends CoapServer {
 
@@ -25,6 +26,8 @@ public class Server extends CoapServer {
     }
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+
         Server server = new Server();
         server.start();
     }
